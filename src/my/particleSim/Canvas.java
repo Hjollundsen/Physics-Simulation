5b73bf3f593d4e9d9dc333eb7edd694fdf3f6aa3
Hjollundsen <Lasse.Hjollund@gmail.com>
2025-03-29 22:33:46 +0100

    I have a label!

--- src/my/particleSim/Canvas.java
+++ src/my/particleSim/Canvas.java
@@ -1,3 +1,5 @@
+package my.particleSim;
+
 //Imports
 import java.awt.*;
 import java.awt.event.*;
@@ -12,9 +14,14 @@ public class Canvas {
     private void makeFrame() {
         frame = new JFrame("Particle Simulation");
         Container contentPane = frame.getContentPane();
-
+        JLabel label = new JLabel("I am a label!");
+        contentPane.add(label);
         //Finally packing frame and making it visible.
         frame.pack();
         frame.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        new Canvas();
     }
 }
